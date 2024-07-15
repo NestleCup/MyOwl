696956c068ff4bc6737b98a09e92dea8b3e9e9cc
Anastasya <NansyCup18@gmail.com>
2024-07-15 18:37:58 +0300

    adding classnames and buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
+import Button from '@/components/Button/Button';
 import { Htag } from '@/components/Htag/Htag';
 
 export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag="h3">Текст</Htag>
+      <Button appearance="primary">Кнопка</Button>
+      <Button appearance="ghost">Кнопка</Button>
     </>
   );
 }
