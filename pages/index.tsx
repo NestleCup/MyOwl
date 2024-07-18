466eae77d57a624f6915c1343ff710cc687b7ded
Anastasya <NansyCup18@gmail.com>
2024-07-18 22:02:58 +0300

    add Tag component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,31 @@
 import Button from '@/components/Button/Button';
 import { Title } from '@/components/Title/Title';
 import { Paragraph } from '@/components/Paragraph/Paragraph';
+import { Tag } from '@/components';
 export default function Home(): JSX.Element {
   return (
     <>
       <Title tag="h3">Текст</Title>
       <Button appearance="primary">Кнопка</Button>
       <Button appearance="ghost">Кнопка</Button>
-      <Paragraph size="s">Маленький</Paragraph>
+      <Paragraph>Маленький</Paragraph>
       <Paragraph size="m">Маленький</Paragraph>
-      <Paragraph size="l">Маленький</Paragraph>
+      <Paragraph>Маленький</Paragraph>
+      <Tag size="small" color="ghost">
+        hh.ru
+      </Tag>
+      <Tag size="medium" color="red">
+        hh.ru
+      </Tag>
+      <Tag size="small" color="grey">
+        HH.ru
+      </Tag>
+      <Tag size="medium" color="green">
+        HH.ru
+      </Tag>
+      <Tag size="medium" color="primary">
+        HH.ru
+      </Tag>
     </>
   );
 }
