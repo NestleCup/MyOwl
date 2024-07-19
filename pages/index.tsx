a7ea53d5ec0d4c08d265fc46017973eba988d755
Anastasya <NansyCup18@gmail.com>
2024-07-19 17:33:38 +0300

    add plugin react-hooks

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,26 @@ import Button from '@/components/Button/Button';
 import { Title } from '@/components/Title/Title';
 import { Paragraph } from '@/components/Paragraph/Paragraph';
 import { Tag } from '@/components';
+import { useEffect, useState } from 'react';
+
 export default function Home(): JSX.Element {
+  const [counter, setCounter] = useState<number>(0);
+  useEffect(() => {
+    console.log('counter' + counter);
+    return function cleanup() {
+      console.log('Unmount');
+    };
+  }, []);
   return (
     <>
+      {counter}
       <Title tag="h3">Текст</Title>
-      <Button appearance="primary">Кнопка</Button>
-      <Button appearance="ghost">Кнопка</Button>
+      <Button appearance="primary" onClick={() => setCounter((x) => x + 1)}>
+        Кнопка
+      </Button>
+      <Button appearance="ghost" onClick={() => setCounter((x) => x - 1)}>
+        Кнопка
+      </Button>
       <Paragraph>Маленький</Paragraph>
       <Paragraph size="m">Маленький</Paragraph>
       <Paragraph>Маленький</Paragraph>
